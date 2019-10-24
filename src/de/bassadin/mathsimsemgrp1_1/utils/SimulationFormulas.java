5abd3c6c692e8637fa033d590290de7b0ea0e704
Tawan <nansintawan99@gmail.com>
2019-10-24 15:34:33 +0200

    speed caculation
    I dont know what I'm doing, but please work.

---
+++ src/de/bassadin/mathsimsemgrp1_1/utils/SimulationFormulas.java
@@ -0,0 +1,19 @@
+package de.bassadin.mathsimsemgrp1_1.utils;
+
+import de.bassadin.mathsimsemgrp1_1.Objects2D.Object2D;
+import de.bassadin.mathsimsemgrp1_1.Objects2D.Quad;
+import de.bassadin.mathsimsemgrp1_1.Objects2D.QuadBody;
+
+public class SimulationFormulas {
+
+    float startDistant;
+    float startSpeed1;
+    float startSpeed2;
+
+    public static float speed(float mass1, float mass2, float startSpeed1, float startSpeed2, float deltaTime){
+        float speed = (1 / mass1 + mass2) * ((mass1 * (startSpeed1 * deltaTime) + (mass2 * (startSpeed2 * deltaTime)));
+        return speed;
+    }
+    
+
+}
