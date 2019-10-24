315f43d8a8ad72121bf99d09a70aa89e6c1b3165
Tawan <nansintawan99@gmail.com>
2019-10-24 15:44:17 +0200

    Update SimulationFormulas.java
    deleted SImulationFormulas field variables

--- src/de/bassadin/mathsimsemgrp1_1/utils/SimulationFormulas.java
+++ src/de/bassadin/mathsimsemgrp1_1/utils/SimulationFormulas.java
@@ -6,10 +6,6 @@ import de.bassadin.mathsimsemgrp1_1.Objects2D.QuadBody;
 
 public class SimulationFormulas {
 
-    float startDistant;
-    float startSpeed1;
-    float startSpeed2;
-
     public static float speed(float mass1, float mass2, float startSpeed1, float startSpeed2, float deltaTime){
         float speed = (1 / mass1 + mass2) * ((mass1 * (startSpeed1 * deltaTime) + (mass2 * (startSpeed2 * deltaTime))));
         return speed;
