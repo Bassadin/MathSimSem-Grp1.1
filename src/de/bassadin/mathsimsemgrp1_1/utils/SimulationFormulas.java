7079e19fbf6a2797f452a610cb3334ee729e0d90
Tawan <nansintawan99@gmail.com>
2019-10-24 15:35:30 +0200

    Update SimulationFormulas.java
    Forget )

--- src/de/bassadin/mathsimsemgrp1_1/utils/SimulationFormulas.java
+++ src/de/bassadin/mathsimsemgrp1_1/utils/SimulationFormulas.java
@@ -11,9 +11,9 @@ public class SimulationFormulas {
     float startSpeed2;
 
     public static float speed(float mass1, float mass2, float startSpeed1, float startSpeed2, float deltaTime){
-        float speed = (1 / mass1 + mass2) * ((mass1 * (startSpeed1 * deltaTime) + (mass2 * (startSpeed2 * deltaTime)));
+        float speed = (1 / mass1 + mass2) * ((mass1 * (startSpeed1 * deltaTime) + (mass2 * (startSpeed2 * deltaTime))));
         return speed;
     }
-    
+
 
 }
