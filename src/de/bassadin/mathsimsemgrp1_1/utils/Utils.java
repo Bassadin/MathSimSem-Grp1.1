03489c494ed79dced447e1434cff13b62f171630
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-03 22:37:40 +0100

    Update Utils.java

--- src/de/bassadin/mathsimsemgrp1_1/utils/Utils.java
+++ src/de/bassadin/mathsimsemgrp1_1/utils/Utils.java
@@ -1,7 +1,7 @@
 package de.bassadin.mathsimsemgrp1_1.utils;
 
 public class Utils {
-    public static double reducedMass(float mass1, float mass2) {
+    public static float reducedMass(float mass1, float mass2) {
         return (mass1 * mass2) / (mass1 + mass2);
     }
 }
