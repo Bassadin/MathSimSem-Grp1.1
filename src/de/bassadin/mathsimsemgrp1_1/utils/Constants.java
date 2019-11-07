83e7df9e6d425f2fdafb3c923551b2083f4cb67e
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-07 15:36:25 +0100

    reduced fps

--- src/de/bassadin/mathsimsemgrp1_1/utils/Constants.java
+++ src/de/bassadin/mathsimsemgrp1_1/utils/Constants.java
@@ -3,7 +3,7 @@ package de.bassadin.mathsimsemgrp1_1.utils;
 public class Constants {
     public static int WINDOW_WIDTH = 800;
     public static int WINDOW_HEIGHT = 600;
-    public static int FPS = 120;
+    public static int FPS = 60;
     public static double TPF = 1.0 / FPS;
     
     //Spring Constant
