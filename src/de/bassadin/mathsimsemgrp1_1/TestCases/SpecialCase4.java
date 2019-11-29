b1f36d9699c11ccde8fe9f606fc496ee1e8fd352
Bastian Hodapp <bastianhodapp@gmail.com>
2019-11-29 11:16:41 +0100

    Create SpecialCase4.java

---
+++ src/de/bassadin/mathsimsemgrp1_1/TestCases/SpecialCase4.java
@@ -0,0 +1,23 @@
+package de.bassadin.mathsimsemgrp1_1.TestCases;
+
+import de.bassadin.mathsimsemgrp1_1.BaseSpringSimulation;
+import de.bassadin.mathsimsemgrp1_1.utils.Constants;
+
+public class SpecialCase4 {
+    public static void main(String[] args) {
+        //Same Weight
+        //No start speed
+        //Equilibrium distance is equal to masses distance
+        // -> Bodies move ('worm' like movement), center of mass moves in positive x direction
+        BaseSpringSimulation testObjekt = new BaseSpringSimulation(
+                Constants.testCaseDefaultMass,
+                Constants.testCaseDefaultMass,
+                18,
+                300,
+                100,
+                400,
+                50,
+                0
+        );
+    }
+}
