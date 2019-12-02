e589e3cd88ac1c67ba71689028a0aba0eb778142
Bastian Hodapp <bastianhodapp@gmail.com>
2019-12-02 09:12:40 +0100

    Made all constants final

--- src/de/bassadin/mathsimsemgrp1_1/utils/Constants.java
+++ src/de/bassadin/mathsimsemgrp1_1/utils/Constants.java
@@ -1,17 +1,17 @@
 package de.bassadin.mathsimsemgrp1_1.utils;
 
 public class Constants {
-    public static int WINDOW_WIDTH = 1000;
-    public static int WINDOW_HEIGHT = 600;
-    public static int FPS = 60;
-    public static double TPF = 1.0 / FPS;
+    public static final int WINDOW_WIDTH = 1000;
+    public static final int WINDOW_HEIGHT = 600;
+    public static final int FPS = 60;
+    public static final double TPF = 1.0 / FPS;
 
     //Mass display
-    public static double massSizeMultiplicator = 14;
+    public static final double massSizeMultiplicator = 14;
 
     //Test case standard values
-    public static float testCaseDefaultMass = 11f;
+    public static final float testCaseDefaultMass = 11f;
     
     //Spring Constant
-    public static double springConstant = 5.0;
+    public static final double springConstant = 5.0;
 }
